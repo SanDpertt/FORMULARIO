931bab2c76ca5729276a6f0eedee78c71445a540
SanDpertt <100301861+SanDpertt@users.noreply.github.com>
2022-06-09 15:39:06 -0500

    Add files via upload

---
+++ Examen_Santiago/java.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("formulario");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+})
+
+function insertRowtable() {
+
+    let Table = document.getElementById("Cuadro");
+    let newLineRow = Table.insertRow(-1);
+    let newCelda = newLineRow.insertCell(0);
+
+    var resultado;
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = (document.getElementById("n2").value);
+    var n3 = parseInt(document.getElementById("n3").value);
+    var n4 = parseInt(document.getElementById("n4").value);
+
+    if (n4 >= 20000){
+        resultado = ((n4 * 0.2 ) - (n4))
+    }
+    else {
+        resultado = n4
+    }
+
+    newCelda.textContent = n1;
+    let newCelda2 = newLineRow.insertCell(1);
+    newCelda2.textContent = n2;
+    let newCelda3 = newLineRow.insertCell(2);
+    newCelda3.textContent = n3;
+    let newCelda4 = newLineRow.insertCell(3);
+    newCelda4.textContent = resultado;
+}
